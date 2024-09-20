d76dbd3faa8438af8a0f95e10dbfcbe575bf1a67
atdilakshan <dilakshan0501@gmail.com>
2024-09-20 11:01:55 +0530

    Basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,130 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import {
+  NgClass,
+  NgStyle,
+  NgIf,
+  NgFor,
+  NgSwitch,
+  NgSwitchCase,
+  NgSwitchDefault,
+  DatePipe,
+  LowerCasePipe,
+  UpperCasePipe,
+  CurrencyPipe,
+  PercentPipe,
+} from '@angular/common';
+import { AppendPipe } from './pipes/append.pipe';
+import { DataService } from './services/data.service';
+import { Data } from './interfaces/data';
+import { response } from 'express';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    NgClass,
+    NgStyle,
+    NgIf,
+    NgFor,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    DatePipe,
+    LowerCasePipe,
+    UpperCasePipe,
+    CurrencyPipe,
+    PercentPipe,
+    AppendPipe,
+  ],
+  providers: [DataService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-app';
+
+  // data binding
+  myBTN: string = 'My Button';
+
+  // attr binding
+  isDisable: boolean = true;
+  angularImage: string = 'images/angular_gradient.png';
+  angularImageAlt: string = 'Angular image';
+
+  // style binding
+  bgColor: string = '#333';
+  titleColor: string = 'red';
+  description: string = 'font-size: 50px; color: #999';
+
+  // class binding
+  redText: string = 'abcd';
+
+  // Functions
+  counter: number = 0;
+
+  incrementCounter() {
+    this.counter++;
+  }
+  decrementCounter() {
+    this.counter--;
+  }
+
+  // two way data binding
+  inputText: string = 'Initial value';
+
+  // classes and styles
+  message: string = 'This is message';
+  classes: string = 'danger text-size';
+
+  selectedColor: string = 'green';
+
+  // Structure Directives ｜ ngIf ngFor and ngSwitch
+  // ngIf
+  isLoggedIn: boolean = false;
+  check: boolean = true;
+
+  // ngFor
+  nameArray: string[] = ['Dilakshan', 'Dilak', 'Dilaks'];
+
+  // ngSwitch
+  grade: string = 'C';
+
+  // Control flow | @If @for @switch
+  a: number = 1;
+  b: number = 2;
+
+  items = [
+    { id: 1, name: 'item1' },
+    { id: 2, name: 'item2' },
+    { id: 3, name: 'item3' },
+  ];
+
+  // Pipes | Built-in pipes and custom pipes
+  // Built-in pipes
+  pipeText: string = 'Hello';
+  today: number = Date.now();
+  currency: number = 1.1234;
+
+  // Dependency Injection and Services for API Calls with HttpClient
+  data: string[] = [];
+  posts: Data[] = [];
+
+  constructor(private dataService: DataService) {
+    this.data = this.dataService.getData();
+  }
+
+  ngOnInit() {
+    this.dataService.getPosts().subscribe({
+      next: (response: Data[]) => {
+        this.posts = response;
+      },
+      error: (error: Error) => {
+        console.log(error);
+      },
+    });
+  }
 }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Data } from '../interfaces/data';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: HttpClient) {}
+
+  getData() {
+    return ['data1', 'data2', 'data3'];
+  }
+
+  getPosts(): Observable<Data[]> {
+    var data = this.http.get<Data[]>(this.apiUrl);
+    return data;
+  }
+}
